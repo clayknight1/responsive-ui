11529dd4194547cca645741d82e9e01b60e87d87
clay knight <Clay@clays-Mac-mini.local>
2018-03-15 17:44:44 -0700

    first commit

---
+++ main.js
@@ -0,0 +1,8 @@
+
+
+window.onload = function () {
+    document.querySelector("#nav-toggle")
+        .addEventListener("click", function () {
+            this.classList.toggle("active");
+        });
+}
